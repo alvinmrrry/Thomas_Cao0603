8c6299bed5477b2698eaeaf452e1a2ad059d97f4
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-15 11:09:21 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,20 @@ import time
 
 st.title("Streamlit Write Stream Example")
 
+st.sidebar("Streamlit Write Stream Example")
+
 # Stream data generation
 def generate_data():
+    """
+    Generates a sequence of strings, with a delay of 1 second between each generation.
+    
+    Yields:
+        str: A string of the form "Stream data point: <i>"
+    """
     for i in range(10):
         yield f"Stream data point: {i}"
         time.sleep(1)  # Simulating delay for streaming
 
-st.sidebar("Streamlit Write Stream Example")
 
 # Using the st.write_stream function
 if st.button("Start Streng"):
