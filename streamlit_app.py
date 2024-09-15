dca83bf0accb048127e35718b71dcc64188af408
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-15 12:35:48 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,18 @@
-import requests  # 添加请求库
+import requests  # 添加请求
+import json
 import streamlit as st
 
-# 使用请求从网络获取数据
-try:
-    response = requests.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY')  # 替换为实际的API URL
-    response.raise_for_status()  # 检查请求是否成功
-    data = response.json()  # 假设返回的是JSON格式的数据
-    st.write(data)  # 使用Streamlit打印内容
-except requests.exceptions.RequestException as e:
-    st.error(f"请求失败: {e}")  # 打印错误信息
+# 使用请求从网络获取数据 2203d27aa32a1d92275134fb632bf009714b2476
+url = "https://google.serper.dev/search"
+
+payload = json.dumps({
+  "q": "apple inc"
+})
+headers = {
+  'X-API-KEY': '2203d27aa32a1d92275134fb632bf009714b2476',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+st.write(response)
