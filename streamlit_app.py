bf81e7dd5a04c1f0f4f9635a406b890fa348f364
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-15 11:04:37 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ def generate_data():
         yield f"Stream data point: {i}"
         time.sleep(1)  # Simulating delay for streaming
 
+st.sidebar("Streamlit Write Stream Example")
+
 # Using the st.write_stream function
 if st.button("Start Streng"):
     with st.empty():  # Creates an empty container
