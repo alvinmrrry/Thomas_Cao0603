d42f05a0f85f6a339da889bd6de225b66b7c7fc3
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-15 11:25:29 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import time
 
+# Sidebar content
 with st.sidebar():
-    st.write('asdfasfsafd')
+    st.title('Sidebar Title')
+    st.write('This is a sidebar')
+    option = st.radio('Choose an option:', ['Option 1', 'Option 2', 'Option 3'])
 
-st.title("Streamlit Write Stream Example")
+# Main content based on sidebar selection
+if option == 'Option 1':
+    st.write('You selected Option 1')
+elif option == 'Option 2':
+    st.write('You selected Option 2')
+elif option == 'Option 3':
+    st.write('You selected Option 3')
 
-
+st.title('Streamlit Write Stream Example')
 
 # Stream data generation
 def generate_data():
@@ -20,9 +29,8 @@ def generate_data():
         yield f"Stream data point: {i}"
         time.sleep(1)  # Simulating delay for streaming
 
-
-# Using the st.write_stream function
-if st.button("Start Streng"):
+# Using the st.button function
+if st.button('Start Stream'):
     with st.empty():  # Creates an empty container
         for piece in generate_data():
             st.write(piece)
