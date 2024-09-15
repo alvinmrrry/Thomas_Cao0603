2d0d8671e68b9f99a5302d0f3f8219797fbd88b3
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-15 12:47:43 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,18 @@ import streamlit as st
 # 使用请求从网络获取数据 2203d27aa32a1d92275134fb632bf009714b2476
 url = "https://google.serper.dev/search"
 
+query = st.text_input('Please input the query:')
 
+if query:
+    payload = json.dumps({
+    "q": query
+    })
 
-payload = json.dumps({
-  "q": st.text_input('Please input the query:')
-})
-headers = {
-  'X-API-KEY': '2203d27aa32a1d92275134fb632bf009714b2476',
-  'Content-Type': 'application/json'
-}
+    headers = {
+    'X-API-KEY': '2203d27aa32a1d92275134fb632bf009714b2476',
+    'Content-Type': 'application/json'
+    }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-st.write(response.text)
+    st.write(response.text)
