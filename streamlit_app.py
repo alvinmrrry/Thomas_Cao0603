47e7611310f2188440ca8bbbb5da1aa290a6c8bb
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-15 10:34:44 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My first new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let uss start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
