1116ca81fdf407ee3b108c7224f8cdea55ab5c47
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-16 18:16:47 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,66 +19,61 @@ if uploaded_file is not None:
     with open(temp_file_path, "wb") as f:
         f.write(uploaded_file.getvalue())
 
-    # Display the file path
-    st.write(f"File Path: {temp_file_path}")
-
-
-# image encoding
-# image_path = 'original.jpg'
-# def encode_image(image_path):
-#     """
-#     Encodes an image file into a base64 string.
+image_path = './' + uploaded_file.name
+def encode_image(image_path):
+    """
+    Encodes an image file into a base64 string.
     
-#     Args:
-#         image_path (str): The path to the image file.
+    Args:
+        image_path (str): The path to the image file.
     
-#     Returns:
-#         str: The base64 encoded string of the image.
-#     """
-#     try:
-#         with open(image_path, "rb") as image_file:
-#             encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
-#             return encoded_string
-#     except FileNotFoundError:
-#         raise ValueError(f"File {image_path} not found.")
-#     except IOError:
-#         raise ValueError(f"Error reading file {image_path}.")
-#     except Exception as e:
-#         raise ValueError(f"An error occurred: {e}") from e
+    Returns:
+        str: The base64 encoded string of the image.
+    """
+    try:
+        with open(image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+            return encoded_string
+    except FileNotFoundError:
+        raise ValueError(f"File {image_path} not found.")
+    except IOError:
+        raise ValueError(f"Error reading file {image_path}.")
+    except Exception as e:
+        raise ValueError(f"An error occurred: {e}") from e
 
-# if uploaded_file:
-#     base64_image = encode_image(uploaded_file)
+if uploaded_file:
+    base64_image = encode_image(uploaded_file)
 
-# # image to text function
-# def image_to_text(client, model, base64_image, prompt):
-#     if not client:
-#         raise ValueError("Groq client cannot be None")
-#     if not model:
-#         raise ValueError("Model cannot be None")
-#     if not base64_image:
-#         raise ValueError("Base64 image cannot be None")
-#     if not prompt:
-#         raise ValueError("Prompt cannot be None")
-#     try:
-#         chat_completion = client.chat.completions.create(
-#             messages = [
-#                 {"role": "user",
-#                  "content": [
-#                      {"type":"text", "text":prompt},
-#                      {"type":"image_url",
-#                       "image_url":{
-#                           "url":f"data:image/jpg;base64,{base64_image}",
-#                       }}
-#                  ]
-#                 }
-#             ],
-#             model = model
-#         )
-#     except Exception as e:
-#         raise RuntimeError(f"Error calling Groq API: {e}") from e
-#     return chat_completion.choices[0].message.content
+# image to text function
+def image_to_text(client, model, base64_image, prompt):
+    if not client:
+        raise ValueError("Groq client cannot be None")
+    if not model:
+        raise ValueError("Model cannot be None")
+    if not base64_image:
+        raise ValueError("Base64 image cannot be None")
+    if not prompt:
+        raise ValueError("Prompt cannot be None")
+    try:
+        chat_completion = client.chat.completions.create(
+            messages = [
+                {"role": "user",
+                 "content": [
+                     {"type":"text", "text":prompt},
+                     {"type":"image_url",
+                      "image_url":{
+                          "url":f"data:image/jpg;base64,{base64_image}",
+                      }}
+                 ]
+                }
+            ],
+            model = model
+        )
+    except Exception as e:
+        raise RuntimeError(f"Error calling Groq API: {e}") from e
+    return chat_completion.choices[0].message.content
 
-# prompt = 'Describe the image'
+prompt = 'Describe the image'
 
-# result = image_to_text(client, llava_model, base64_image, prompt)
-# st.write(result)
+result = image_to_text(client, llava_model, base64_image, prompt)
+st.write(result)
