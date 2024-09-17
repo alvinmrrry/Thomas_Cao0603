3797782c142a73a0508bff71d24c87483e1eae2b
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-17 14:50:28 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,5 +90,5 @@ def process_canvas_result(image_data):
                         {"type": "image_url",
                         "image_url": {
                             "url": f"data:image/jpg;base64,{base64_image}",
-                        }}
-                        
+                        }}}
+                    ]])
