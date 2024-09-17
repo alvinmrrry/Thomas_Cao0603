a7dbe44006b79abd89db9c80b313b8d1bdd1c8dd
alvinmrrry <alvin_mrrry@outlook.com>
2024-09-17 14:06:11 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,11 +79,11 @@ def full_app():
     # Do something interesting with the image data and paths
     if canvas_result.image_data is not None:
         st.image(canvas_result.image_data)
-    if canvas_result.json_data is not None:
-        objects = pd.json_normalize(canvas_result.json_data["objects"])
-        for col in objects.select_dtypes(include=["object"]).columns:
-            objects[col] = objects[col].astype("str")
-        st.dataframe(objects)
+    # if canvas_result.json_data is not None:
+    #     objects = pd.json_normalize(canvas_result.json_data["objects"])
+    #     for col in objects.select_dtypes(include=["object"]).columns:
+    #         objects[col] = objects[col].astype("str")
+    #     st.dataframe(objects)
 
 if __name__ == "__main__":
     st.set_page_config(
